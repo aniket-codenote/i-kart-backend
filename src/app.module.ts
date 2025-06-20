a0e15eb939d092fb6504e8f9c75fbe29841028fb
Aniket Patidar <aniket.codenote@gmail.com>
2025-06-20 18:29:06 +0530

    load .env globally, and validate DATABASE_URL at startup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { PrismaModule } from './prisma/prisma.module';
 import { CatalogsModule } from './catalogs/catalogs.module';
 
 @Module({
-  imports: [PrismaModule, CatalogsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+      }),
+    }),
+    PrismaModule,
+    CatalogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

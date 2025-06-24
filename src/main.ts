56791cd8294b8a39b51811c6d440efe0cd527f7f
Chetan <chetan.codenote@gmail.com>
2025-06-24 16:15:06 +0530

    [main] add cors for allow api fetch.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { createSwaggerDocument } from './swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   createSwaggerDocument(app);
+  app.enableCors({
+    origin: 'http://localhost:3001', // frontend origin
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

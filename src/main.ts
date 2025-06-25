0f50bbebba7705f6ce328adc776444eca4faa304
Aniket Patidar <aniket.codenote@gmail.com>
2025-06-26 00:47:36 +0530

    Bind ValidationPipe at the application level

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { createSwaggerDocument } from './swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     origin: 'http://localhost:3001', // frontend origin
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
